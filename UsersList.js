74c463c1167dd261d9c420886de970e343744204
AleksandrsKudrins <18666264+AleksandrsKudrins@users.noreply.github.com>
2018-12-26 00:01:41 +0200

    Update UsersList.js

--- UsersList.js
+++ UsersList.js
@@ -40,13 +40,13 @@ class UsersList extends Component {
     }
 
     const UsersList = users.map(user => {
-      const userInfo = `${user.name || ''} ${user.email || ''} ${user.description || ''}`;
-      const userStatus = `${user.status}`;
-	  return <tr key={user.id}>
+      const userInfo = `${user.name || ''} ${user.email || ''} ${user.description || ''} ${user.status}`;
+ 	  return <tr key={user.id}>
         <td style={{whiteSpace: 'nowrap'}}>{user.name}</td>
-        <td>{userInfo}</td>
-		<td>{userStatus}</td>
-        <td>
+		<td style={{whiteSpace: 'nowrap'}}>{user.email}</td>
+		<td style={{whiteSpace: 'nowrap'}}>{user.description}</td>
+		<td style={{whiteSpace: 'nowrap'}}>{user.status}</td>
+       <td>
           <ButtonGroup>
             <Button size="sm" color="primary" tag={Link} to={"/users/" + user.id}>Edit</Button>
             <Button size="sm" color="danger" onClick={() => this.remove(user.id)}>Delete</Button>
@@ -63,13 +63,14 @@ class UsersList extends Component {
           <div className="float-right">
             <Button color="success" tag={Link} to="/users/new">Add user</Button>
           </div>
-          <h3>webFinances Users</h3>
+          <h3>Users List</h3>
           <Table className="mt-4">
             <thead>
             <tr>
               <th width="20%">Name</th>
               <th width="20%">E-mail</th>
               <th width="30%">Description</th>
+			  <th width="10%">Status</th>
               <th width="10%">Actions</th>
             </tr>
             </thead>

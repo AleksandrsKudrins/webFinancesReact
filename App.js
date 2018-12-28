05c977bc192fc8479da565b5e573c0e9e0ba3e85
AleksandrsKudrins <18666264+AleksandrsKudrins@users.noreply.github.com>
2018-12-28 12:19:39 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UsersList from './UsersList';
 import UsersEdit from './UsersEdit';
+import OrdersList from './OrdersList';
 
 class App extends Component {
   render() {
-    return (
+  return (
       <Router>
         <Switch>
           <Route path='/' exact={true} component={Home}/>
           <Route path='/users' exact={true} component={UsersList}/>
+          <Route path='/users/:id/orders' exact={true} component={OrdersList}/>
 		  <Route path='/users/:id' component={UsersEdit}/>
         </Switch>
       </Router>
